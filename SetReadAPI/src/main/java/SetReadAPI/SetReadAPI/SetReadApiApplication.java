94f05e39c7ba49880e6c7c2ecfbcf0e8510fa2bc
wyattwyattwyatt <thewyattofgale@gmail.com>
2018-05-23 17:02:30 -0500

    Initial commit

---
+++ SetReadAPI/src/main/java/SetReadAPI/SetReadAPI/SetReadApiApplication.java
@@ -0,0 +1,12 @@
+package SetReadAPI.SetReadAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetReadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetReadApiApplication.class, args);
+	}
+}
---
+++ SetReadAPI/src/test/java/SetReadAPI/SetReadAPI/SetReadApiApplicationTests.java
@@ -0,0 +1,16 @@
+package SetReadAPI.SetReadAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SetReadApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
